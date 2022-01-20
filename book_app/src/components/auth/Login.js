32db08f4a9abffc1bf7a7dc8c4500898558d6fe4
SouthernMinami <nanplays.44inget@gmail.com>
2022-01-20 10:04:02 +0900

    ➕validation, error messageを追加 💻 APIにPOSTリクエスト

--- book_app/src/components/auth/Login.js
+++ book_app/src/components/auth/Login.js
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import {
   Box,
   Button,
@@ -11,6 +13,39 @@ import {
 import { Link } from 'react-router-dom'
 
 export const Login = (props) => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("無効なメールアドレスです。")
+        .required("必須事項です。"),
+      password: Yup.string()
+        .min(4, "パスワードは4〜20文字で指定してください。")
+        .max(20, "パスワードは4~20文字で指定してください。")
+        .matches(/^[a-zA-Z0-9]+$/, "記号はパスワードに使用できません。")
+        .required("必須事項です。"),
+    }),
+    onSubmit: (values) => {
+      const item = values
+      const loginUrl = `https://api-for-missions-and-railways.herokuapp.com/signin`
+      fetch(loginUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(item)
+      }).then(res => res.json())
+        .then(data =>
+          console.log(data.token)
+        )
+        .catch(err =>
+          console.error(err.message)
+        )
+    }
+  })
 
   return (
     <>
@@ -24,14 +59,19 @@ export const Login = (props) => {
           }}
         >
           <Typography variant="h5">{props.title}</Typography>
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
                   id="email"
                   label="メールアドレス"
                   fullWidth
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
                 />
+                {formik.touched.email && formik.errors.email ? (
+                  <div>{formik.errors.email}</div>
+                ) : null}
               </Grid>
               <Grid item xs={12}>
                 <TextField
@@ -39,7 +79,12 @@ export const Login = (props) => {
                   type="password"
                   label="パスワード"
                   fullWidth
+                  onChange={formik.handleChange}
+                  value={formik.values.password}
                 />
+                {formik.touched.password && formik.errors.password ? (
+                  <div>{formik.errors.password}</div>
+                ) : null}
               </Grid>
               <Grid item xs={12}>
                 <Button
--- book_app/src/components/auth/Signup.js
+++ book_app/src/components/auth/Signup.js
@@ -8,9 +8,50 @@ import {
   TextField,
   Typography
 } from '@material-ui/core'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import { Link } from 'react-router-dom'
 
+
 export const Signup = (props) => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(3, "ユーザー名は3~15文字で指定してください。")
+        .max(15, "ユーザー名は3~15文字で指定してください。")
+        .required("必須事項です。"),
+      email: Yup.string()
+        .email("無効なメールアドレスです。")
+        .required("必須事項です。"),
+      password: Yup.string()
+        .min(4, "パスワードは4〜20文字で指定してください。")
+        .max(20, "パスワードは4~20文字で指定してください。")
+        .matches(/^[a-zA-Z0-9]+$/, "記号はパスワードに使用できません。")
+        .required("必須事項です。"),
+    }),
+    onSubmit: (values) => {
+      const item = values
+      const signupUrl = `https://api-for-missions-and-railways.herokuapp.com/users`
+      fetch(signupUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(item)
+      }).then(res => res.json())
+        .then(data =>
+          console.log(data.token)
+        )
+        .catch(err =>
+          console.error(err.message)
+        )
+    }
+  })
 
   return (
     <>
@@ -24,21 +65,31 @@ export const Signup = (props) => {
           }}
         >
           <Typography variant="h5">{props.title}</Typography>
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
                   id="name"
                   label="ユーザー名"
                   fullWidth
+                  onChange={formik.handleChange}
+                  value={formik.values.name}
                 />
+                {formik.touched.name && formik.errors.name ? (
+                  <div>{formik.errors.name}</div>
+                ) : null}
               </Grid>
               <Grid item xs={12}>
                 <TextField
                   id="email"
                   label="メールアドレス"
                   fullWidth
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
                 />
+                {formik.touched.email && formik.errors.email ? (
+                  <div>{formik.errors.email}</div>
+                ) : null}
               </Grid>
               <Grid item xs={12}>
                 <TextField
@@ -46,7 +97,12 @@ export const Signup = (props) => {
                   type="password"
                   label="パスワード"
                   fullWidth
+                  onChange={formik.handleChange}
+                  value={formik.values.password}
                 />
+                {formik.touched.password && formik.errors.password ? (
+                  <div>{formik.errors.password}</div>
+                ) : null}
               </Grid>
               <Grid item xs={12}>
                 <Button
