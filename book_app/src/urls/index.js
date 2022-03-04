5e1330eefc146732cffef03d8591018dee41f0be
SouthernMinami <nanplays.44inget@gmail.com>
2022-03-05 06:49:16 +0900

    📃変数名を小文字に変更

--- book_app/src/urls/index.js
+++ book_app/src/urls/index.js
@@ -1,5 +1,5 @@
 const BASE_URL = `https://api-for-missions-and-railways.herokuapp.com`
 
-export const SIGNUP_URL = `${BASE_URL}/users`
-export const LOGIN_URL = `${BASE_URL}/signin`
-export const BOOK_LIST_URL = `${BASE_URL}/books`
+export const signupUlr = `${BASE_URL}/users`
+export const loginUrl = `${BASE_URL}/signin`
+export const bookListUrl = `${BASE_URL}/books`
